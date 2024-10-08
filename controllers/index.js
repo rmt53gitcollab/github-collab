61e16f385570c27c1b7a6eb8c2b73cca1db0c156
pradana01 <andreanwirapradana@gmail.com>
2024-10-08 14:02:49 +0700

    feat: register
    merge register from development

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,10 @@ class Controller {
     static async login(req, res) {
         res.send("Ini page buat login");
     };
+    
+    static async register(req, res) {
+        res.send("Ceritanya register udah selesai");
+    };
 };
 
 module.exports = Controller;
